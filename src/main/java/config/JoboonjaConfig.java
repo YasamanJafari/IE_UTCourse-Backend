2ca524bf5144588726d35e270553962e167c3369
Yasaman <ys.jafari98@gmail.com>
2019-02-11 17:22:45 +0330

    This commit will add mechanism for handling repeated usernames and project titles.

--- src/main/java/config/JoboonjaConfig.java
+++ src/main/java/config/JoboonjaConfig.java
@@ -3,8 +3,10 @@ package config;
 import user.User;
 
 public class JoboonjaConfig {
-    public static final String USER_NOT_FOUND_ERROR = "user not found";
-    public static final String PROJECT_NOT_FOUND_ERROR = "project not found";
+    public static final String USER_NOT_FOUND_ERROR = "User not found.";
+    public static final String PROJECT_NOT_FOUND_ERROR = "Project not found.";
+    public static final String USERNAME_ALREADY_EXISTS_ERROR = "This username already exists.";
+    public static final String PROJECT_TITLE_ALREADY_EXISTS_ERROR = "This project title already exists.";
     public static String WINNER_MSG(User user)
     {
         if(user == null)
